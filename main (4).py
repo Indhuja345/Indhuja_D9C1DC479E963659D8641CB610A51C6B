7598baf0b397422a794dee0c533e1d74b9121a2c
Indhuja345 <144573661+Indhuja345@users.noreply.github.com>
2023-09-17 13:12:11 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,24 @@
+#to create objects of both the Batsman and Bowler classes and call the play() method for each object.
+class Player:
+
+  def play(self):
+    print("The player is playing cricket.")
+
+
+class Batsman(Player):
+
+  def play(self):
+    print("The batsman is batting.")
+
+
+class Bowler(Player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+batsman = Batsman()
+bowler = Bowler()
+
+batsman.play()
+bowler.play()
